795a490014c9ad78af6f26a51873ba67fcf016f6
Wisit018 <lipton_pink@hotmail.co.th>
2023-12-06 16:02:26 +0700

    /
    /

---
+++ learning/javascript/ES6-Javascript/Block-Scope.js
@@ -0,0 +1,59 @@
+// block scope (let/constant)
+
+// การเขียนแบบเดิม
+
+// var x = 10
+// if (x===10){
+//     var y = 100
+//     console.log("y = "+y);
+// }
+
+// console.log("y = "+y);
+
+// ค่าคงที่
+
+// const x = 50
+// x = 100
+// console.log(x);
+
+// =======================
+// arrow function
+
+// แบบเดิม
+
+// function fullname(fname,lname){
+//     return fname+lname
+// }
+// console.log(fullname("wisit ","phusakul"));
+
+// แบบ ES6
+
+// fullname=(fname,lname)=>fname+lname
+
+// setAge=(age)=>"อายุ = " + age
+
+// console.log(fullname("wisit ","phusakul"));
+
+// console.log(setAge(20))
+
+// =======================
+// ทำงานกับ Object
+
+// const username = "Tarn"
+// const age = 25
+// const address = "BKK"
+
+// const customer = {
+//     username,
+//     age,
+//     address,
+//     showData(){
+//         console.log("ข้อมูลลูกค้าชื่อ "+ username)
+//     }
+// }
+
+// customer.showData()
+
+// =======================
+// string
+
